70a60fca8c5fb08948b4a00d38fdafbdd0ca478a
Tam Nguyen <tnguyen@smith.edu>
2015-09-30 02:24:03 -0400

    changed constructor of selector.js along with naming for x, y attributes, fixed isHit method

--- Assignment 3/Selector.js
+++ Assignment 3/Selector.js
@@ -3,52 +3,42 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-function Selector(numOptions, x, y, minH, maxH, w){
-    CustomEngine.call(this);
-    this.numOptions = numOptions;
-    this.origX = x;
-    this.origY = y;
+function Selector(x, y, minH, maxH, w){
+    this.x = x;
+    this.y = y;
     this.minHeight = minH;
     this.maxHeight = maxH;
     this.width = w;
     this.options = [];
     this.isExpanded = false;
 }
-Selector.prototype = new CustomEngine();
 Selector.prototype.setPosition = function(x, y){
-    this.origX = x;
-    this.origY = y;
+    this.x = x;
+    this.y = y;
 }
-
 Selector.prototype.setWidth = function(w){
     this.width = w;
 }
 Selector.prototype.setMinHeight = function(minH){
     this.minHeight = minH;
 }
-
-
 Selector.prototype.isHit = function(mousePos){
-    if((mousePos.x > 0  && mousePos.x < this.width) 
-            && (mousePos.y > 0 && mousePos.y < this.maxHeight) ){
-     
+    if((mousePos.x > this.x  && mousePos.x < (this.x + this.width)) 
+            && (mousePos.y > this.y && mousePos.y < (this.y + this.maxHeight))){
         this.isExpanded = true;    
     }
     else{
         this.isExpanded = false;
     } 
 }
-
 Selector.prototype.addOption = function(option){
     this.options.push(option);
-    console.log(option.label);
-    console.log(option.x + " ," + option.y );
 }
 
 Selector.prototype.draw = function(g){
     //override 
     if (this.isExpanded === true){
-        for (var i = 0; i < this.numOptions; i++){
+        for (var i = 0; i < this.options.length; i++){
             this.options[i].drawElement(g);
         }
     }
