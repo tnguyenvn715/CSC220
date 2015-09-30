7e809f29557289215e1e2471632419a92e1a241d
Tam Nguyen <tnguyen@smith.edu>
2015-09-30 02:22:13 -0400

    created addAreaSelector and addTypeSelector in main.js, also initialize a customEngine instance to add these selectors

--- Assignment 3/main.js
+++ Assignment 3/main.js
@@ -3,42 +3,39 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-function addAreaSelector(canvas){
-    var numOpt = data.length;
+function addAreaSelector(){
     var x = 0;
     var y = 0;
     var minHeight = 20;
     var maxHeight = data.length*20;
     var width = 100;
-    var areaSelector = new Selector(numOpt, x, y, minHeight, 
-                            maxHeight, width);
+    var areaSelector = new Selector(x, y, minHeight, maxHeight, width);
     for (var i = 0; i < data.length; i++){
         var label = data[i].getName();
         var opt = new OptionElement(0, i*20, 100, 20, label);
         areaSelector.addOption(opt);
     }
-    areaSelector.initialize(canvas);
+    return areaSelector;
 }
-/*function addTypeSelector(canvas){
-    var numOpt = 2;
+function addTypeSelector(canvas){
     var x = 150;
     var y = 0;
     var minHeight = 20;
     var maxHeight = 40;
     var width = 100;
-    var typeSelector = new SelectorEngine(2, 150, 0, 20, 
-                            40, 100);
+    var typeSelector = new Selector(x, y, minHeight,maxHeight, width);
     var barOption = new OptionElement(x, y, width, minHeight, "Bar Graph");
     var lineOption = new OptionElement(x, y + minHeight, width,
                                         minHeight, "Line Graph");
     typeSelector.addOption(barOption);
     typeSelector.addOption(lineOption);
-    typeSelector.initialize(canvas);
-}*/
+    return typeSelector;
+}
 function initialize(){
     var c = document.getElementById("myCanvas");
-    addAreaSelector(c);
-    //addTypeSelector(c);
-    
+    var customEngine = new CustomEngine();
+    customEngine.addSelector(this.addAreaSelector());
+    customEngine.addSelector(this.addTypeSelector());
+    customEngine.initialize(c);   
 }
 window.onload= initialize;
