6cb5575346bef8e71cd13ed13b36bf260e1c7908
Tam Nguyen <tnguyen@smith.edu>
2015-11-03 03:39:42 -0500

    main class initialize the program

---
+++ Project/main.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function initialize() {
+    
+    var gameLoop = new CustomGameLoop();
+    gameLoop.initialize(document.getElementById("myCanvas"));
+    gameLoop.setCanvasSize(640, 480);
+    //timer.moveSlider();
+    //timer.updatePosition(50, 420);
+    //timer.draw();
+}
+
+window.onload = initialize;
