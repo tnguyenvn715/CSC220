0da49d088af561ef25d36ff50f010d88ad51b56f
Tam Nguyen <tnguyen@smith.edu>
2015-11-06 21:11:07 -0500

    added checkYearValidity method for inputPrompter

--- Project/inputBox.js
+++ Project/inputBox.js
@@ -3,10 +3,32 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+function inputPrompter(minYear, currentYear) {
+    this.isValid = false;
+    this.minYear = minYear;
+    this.currentYear = currentYear;
+    this.year = prompt("Please enter your birth year: ");
+    while (this.isValid === false){
+        this.checkYearValidity();
+    }
+}
 
-function inputBox(){
-
-    this.year = prompt("Please enter your birth year: ", "1993");
-    
+inputPrompter.prototype.checkYearValidity = function() {
+    if (isNaN(this.year) === true) {   
+        this.year = prompt("This is not valid.\n\Please enter again: ");
+    }
+    else if (isNaN(this.year) === false && this.year % 1 != 0) {  
+        this.year = prompt("Your birth year should be an integer.\n\Please enter again: "); 
+    }
+    else if (isNaN(this.year) === false && this.year < this.minYear) {   
+        this.year = prompt("I doubt you are " + (this.currentYear - this.year) + 
+                            " years old.\n\Please enter again: ");
+    }
+    else if (isNaN(this.year) === false  && this.year > this.currentYear ) {    
+        this.year = prompt("I doubt you are born that year.\n\Please enter again: ");
+    }
+    else {
+        this.isValid = true;
+    }
 }
 
