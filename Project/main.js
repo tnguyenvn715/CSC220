783585e5ca8d6530c77183057b96ec45e10d6124
Tam Nguyen <tnguyen@smith.edu>
2015-11-06 21:17:06 -0500

    minor changes in line breaks and spaces

--- Project/main.js
+++ Project/main.js
@@ -1,18 +1,7 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-/*function initialize() {
-    
-    var gameLoop = new CustomGameLoop();
-    gameLoop.initialize(document.getElementById("myCanvas"));
-    gameLoop.setCanvasSize(640, 480);
-    //timer.moveSlider();
-    //timer.updatePosition(50, 420);
-    //timer.draw();
+function initialize() {
+    var customGameLoop = new CustomGameLoop();
+    var canvas =document.getElementById("canvas"); 
+    customGameLoop.initialize(canvas);
 }
 
-window.onload = initialize;*/
+window.onload= initialize;
