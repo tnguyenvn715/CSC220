0fd79592b0311442b82b49563c0da14da4091927
Tam Nguyen <tnguyen@smith.edu>
2015-11-26 22:15:56 -0500

    no message

--- Class Project/Timer.js
+++ Class Project/Timer.js
@@ -3,80 +3,74 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-function Timer(x, y, g, length, startYear, endYear){
-    this.x = x;
-    this.y = y;
+function Timer(g, position, length, startYear, endYear) {
     this.g = g;
+    this.position = position;
     this.length= length;
     this.label = startYear;
     this.startYear = startYear;
     this.endYear = endYear;
 }
-
-Timer.prototype.getX = function(){
-    return this.x;
-}
-
-Timer.prototype.getY = function(){
-    return this.y;
-}
-
-Timer.prototype.getStartYear = function(){
+Timer.prototype.getStartYear = function() {
     return this.startYear;
 }
-
-Timer.prototype.getEndYear = function(){
+Timer.prototype.getEndYear = function() {
     return this.endYear;
 }
-
 Timer.prototype.getLabel = function() {
     return this.label;
 }
-Timer.prototype.updateYearLabel = function(newYear) {
+Timer.prototype.setLabel = function(newYear) {
     this.label = newYear;
 }
-Timer.prototype.setX = function(x) {
-    this.x = x;
+Timer.prototype.getPosition = function() {
+    return this.position.clone();
 }
-Timer.prototype.setY = function(y) {
-    this.y = y;
+Timer.prototype.setPosition = function(position) {
+    this.position.x = position.getX();
+    this.position.y = position.getY();
 }
-Timer.prototype.setLength = function(length) {
+Timer.prototype.getPixelLength = function() {
+    return this.length;
+}
+Timer.prototype.setPixelLength = function(length) {
     this.length = length;
 }
-Timer.prototype.getScaleIncrement = function() {
+Timer.prototype.getHorizontalncrement = function() {
     var increment = this.length/(parseInt(this.endYear)
                         - parseInt(this.startYear));
     return increment;
 }
-Timer.prototype.updatePosition = function(x, y) {
-    this.x = x;
-    this.y = y;
-}
-Timer.prototype.moveSlider = function() {
-    var increment = this.getScaleIncrement();
+
+Timer.prototype.moveSlider = function(g) {
+    var increment = this.getHorizontalncrement();
     
-    if (parseInt(this.label) >= 2015) {
-        return null;
-    }
-    else {
-        var nextYear = parseInt(this.label) + 1;
-        var newx = this.x + increment; 
-        this.updatePosition(newx, this.y);
-        this.updateYearLabel(nextYear);
-        document.getElementById("year").innerHTML = this.label ;
+    if (this.label < this.endYear) {
+        
+        var nextYear = this.label + 1;
+        var newx = this.position.getX() + increment;
+        var newPosition = new Point(newx, this.position.getY());
+        this.setPosition(newPosition);
+        this.setLabel(nextYear);
+        
     }  
+    else{
+        this.label = this.startYear;
+        var newPosition = new Point(30, this.position.getY() );
+        this.setPosition(newPosition);
+        this.setLabel(this.startYear);
+
+   
+    }
 }
-Timer.prototype.drawScale = 
-        function(g, x, y, length, height, radius, fill, stroke) {
+Timer.prototype.drawTimerBar = 
+        function(g, position, length, height, fill, stroke) {
     if (typeof stroke == "undefined" ) {
         stroke = true;
     }
-    if (typeof radius === "undefined") {
-        radius = 5;
-    }
+    g.save();
     g.beginPath();
-    g.rect(x, y,length, height);
+    g.rect(position.getX(), position.getY(),length, height);
     g.closePath();
     g.fillStyle = 'gray'; 
     g.lineWidth = 1;
@@ -86,24 +80,38 @@ Timer.prototype.drawScale =
     }
     if (fill) {
         g.fill();
-    }        
+    }       
+    g.restore();
 }
 
 Timer.prototype.drawLabel = function(g) {
+    g.save();
     g.fillStyle = 'black';
     g.font = '12pt Times'; 
-    g.fillText(this.label, this.x - 20 ,this.y + 25);
+    var xpos = this.position.getX() - 20;
+    var ypos = this.position.getY() + 25;
+    g.fillText(this.label, xpos , ypos);
+    g.restore();
 }
 Timer.prototype.draw = function(g) {
-    this.drawScale(g, 30, this.y , this.length, 5, 10, true);
+    var timerBarPosition = new Point(30, this.position.getY());
+    this.drawTimerBar(g, timerBarPosition, this.length, 5, true);
     this.drawLabel(g);
-    g.beginPath();
-    g.arc(this.x, this.y, 10, 0, 2 * Math.PI, false);
-    g.fillStyle = 'red';
-    g.fill();
-    g.lineWidth = 1;
-    g.strokeStyle = 'black';
-    g.stroke();
-    
+    this.drawSlider(g, 'black', 'black');
+    this.updateHTMLLabel(this.label);
 }
 
+Timer.prototype.drawSlider = function(g, fillColor, strokeColor){
+    g.save();
+    g.beginPath();
+    g.arc(this.position.getX(), this.position.getY(), 10, 0, 2 * Math.PI, false);
+    g.fillStyle = fillColor;
+    g.fill();
+    g.lineWidth = 1;
+    g.strokeStyle = strokeColor;
+    g.stroke();
+    g.restore;
+}
+Timer.prototype.updateHTMLLabel = function(label){
+    document.getElementById("year").innerHTML = label ;
+}
--- Class Project/nbproject/main.js
+++
@@ -1,7 +0,0 @@
-function initialize() {
-    var customGameLoop = new CustomGameLoop();
-    var canvas =document.getElementById("canvas"); 
-    customGameLoop.initialize(canvas);
-}
-
-window.onload= initialize;
